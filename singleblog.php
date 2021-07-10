e2a4324af000a931966ad188cf51ded554e2a030
Luana <luana@localhost.localdomain>
2021-07-10 17:26:14 +0300

    single blog post

---
+++ singleblog.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php' ?>
+<body data-spy="scroll" data-target=".navbar" data-offset="90">
+
+    <!-- Loader -->
+<?php include 'loader.php' ?>
+<!-- Loader ends -->
+
+    <!-- Page Header -->
+    <section class="page_header pb-0 w-100">
+      <div class="container">
+          <div class="row">
+              <div class="col-md-12 page-content position-relative">
+                  <h2 class="text-white mt-5 mb-2">Our Blog</h2>
+                  <p class="text-white">We offer the most creative web designs.</p>
+                  <div class="page_nav bg-white d-inline-block">
+                      <span>You are here:</span> <a href="index.html" class="d-inline-block color-black">Home</a> <span class="color-summer-sky"><i class="ti ti-angle-double-right"></i>Blog</span>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </section>
+    <!-- Page Header -->
+
+    <!-- Blog starts -->
+    <section class="bg-light-gray text-left">
+     <div class="container">
+       <div class="row">
+           <div class="col-lg-12">
+                   <!-- Blog List Inner -->
+                   <div class="blog-listing-inner heading-space">
+                       <div class="image hover-effect">
+                           <img src="images/banner-blog.jpg" alt="blog-img">
+                       </div>
+               </div>
+           </div>
+
+           <!-- Blog Left Listing -->
+         <div class="col-lg-8 col-md-7">
+             <div class="blog-box heading-space m-md-0">
+                 <!-- Blog List Inner -->
+                 <div class="blog-listing-inner news_item">
+                     <div class="news_desc">
+                         <h3 class="text-capitalize font-weight-500"><a href="blog-detail.html" class="color-black">How To Build A Virtual Reality Model With A Real-Time Cross-Device Preview.</a></h3>
+                         <p class="mb-35px color-grey line-height-25px">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when a printer took a galley.</p>
+                         <p class="mb-40px">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec facilisis nullano volutpat justo. Nulla tempus ultricies feugiat. Duis magna risus, luctus id urna dapibus condimentum dui. Maecenas tempor non ex eu vehicula. Sed sit placerat velit.</p>
+                         <blockquote class="mb-50px blockquote font-italic font-weight-light color-black"><i class="ti ti-quote-left font-30 pr-2"></i>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley</blockquote>
+                         <ul class="rounded heading-space">
+                             <li>Lorem Ipsum has been the industry's standard dummy text ever</li>
+                             <li>Lorem Ipsum has been the industry's standard dummy text ever</li>
+                             <li>Lorem Ipsum has been the industry's standard dummy text ever</li>
+                             <li>Lorem Ipsum has been the industry's standard dummy text ever</li>
+                             <li>Lorem Ipsum has been the industry's standard dummy text ever</li>
+                         </ul>
+
+                         
+                     </div>
+                 </div>
+             </div>
+         </div>
+         <!-- Blog Widgets -->
+         <div class="col-lg-4 col-md-5">
+             <div class="text-left">
+                 <!-- Search Box -->
+                
+                 <!-- Recent Post -->
+                
+                 <!-- Categories -->
+                 
+                 <!-- Tags -->
+                 
+             </div>
+         </div>
+       </div>
+     </div>
+    </section>
+    <!-- Blog ends -->
+
+<!-- Footer starts -->
+<?php include 'footer.php' ?>
+<!-- Footer ends -->
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<!-- Bootstrap JS File -->
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Morphtext JS File -->
+<script src="js/morphext.min.js"></script>
+<!-- Flip JS File -->
+<script src="js/jquery.flip.min.js"></script>
+<!-- Appear JS File -->
+<script src="js/jquery.appear.js"></script>
+    <!-- Megamenu Js File -->
+    <script src="js/megamenu.js"></script>
+<!-- Isotop gallery -->
+<script src="js/isotope.pkgd.min.js"></script>
+<!-- Cube Portfolio JS File -->
+<script src="js/jquery.cubeportfolio.min.js"></script>
+<!-- Equal Height JS File -->
+<script src="js/jquery.matchHeight-min.js"></script>
+<!--Parallax Background-->
+<script src="js/parallaxie.min.js"></script>
+<!-- Fancy Box JS File -->
+<script src="js/jquery.fancybox.min.js"></script>
+<!-- Swiper JS File -->
+<script src="js/swiper.min.js"></script>
+<!-- Owl Carousel JS File -->
+<script src="js/owl.carousel.js"></script>
+<!-- Wow JS File -->
+<script src="js/wow.js"></script>
+<!-- Headroom Nav File -->
+<script src="js/headroom.js"></script>
+
+    <!--Revolution Slider-->
+<script src="js/revolution/jquery.themepunch.tools.min.js"></script>
+<script src="js/revolution/jquery.themepunch.revolution.min.js"></script>
+<script src="js/revolution/jquery.themepunch.revolution.contdown.min.js"></script>
+
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  (Load Extensions only on Local File Systems !  The following part can be removed on Server for On Demand Loading) -->
+<script src="js/revolution/extensions/revolution.extension.actions.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.carousel.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.kenburn.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.layeranimation.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.migration.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.navigation.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.parallax.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.slideanims.min.js"></script>
+<script src="js/revolution/extensions/revolution.extension.video.min.js"></script>
+
+<!-- Custom JS File -->
+<script src="js/functions.js"></script>
+</body>
+</html>
+
+
