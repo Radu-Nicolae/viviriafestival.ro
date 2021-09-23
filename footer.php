8e6163379ac605cb6b7e49bd635d8c139d622331
Radu-Nicolae <rnicolae04@gmail.com>
2021-09-23 21:47:15 +0000

    Footer finished

--- footer.php
+++ footer.php
@@ -11,11 +11,11 @@
 <!--                    <li><a href="javascript:void(0)" class="wow fadeInDown pinterest"><i class="ti ti-mobile"></i>-->
 <!--                        </a></li>-->
 <!--                    <br>-->
-                    <li><a href="https://m.facebook.com/Viviria-Festival-104340941971474" class="wow fadeInUp facebook"><i class="ti ti-facebook"></i> </a>
+                    <li><a href="https://m.facebook.com/Viviria-Festival-104340941971474" target="_blank" class="wow fadeInUp facebook"><i class="ti ti-facebook"></i> </a>
                     </li>
-                    <li><a href="geo:44.472333, 26.076362" class="wow fadeInUp google"><i class="ti ti-map"></i> </a></li>
+                    <li><a href="geo:44.472333, 26.076362" target="_blank" class="wow fadeInUp google"><i class="ti ti-map"></i> </a></li>
 
-                    <li><a href="https://www.instagram.com/viviria.festival/" class="wow fadeInUp instagram"><i class="ti ti-instagram"></i> </a>
+                    <li><a href="https://www.instagram.com/viviria.festival/" target="_blank" class="wow fadeInUp instagram"><i class="ti ti-instagram"></i> </a>
                     </li>
 
                 </ul>
